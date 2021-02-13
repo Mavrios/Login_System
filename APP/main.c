f86bb28d15912552e35bb13e785449e1d8015e8d
Ahmed Kishk <ahmedadelkishk@gmail.com>
2021-02-13 06:05:39 +0200

    Adding Password Part

--- APP/main.c
+++ APP/main.c
@@ -19,30 +19,31 @@
 #define PASS_Length	3
 #define PASS_Number	3
 
-#define ID1			'ABC'
-#define ID2			'CBA'
-#define ID3			'ACB'
+#define Tries 		3
 
-#define u8Pass1		'123'
-#define u8Pass2		'321'
-#define u8Pass3		'132'
+
 
 #define ID_Flag		7
 #define Pass_Flag	6
 void voidClearArr(u8 *Copy_u8Ptr);
 u8 u8CheckID(u8 *Copy_u8Ptr, u8 Copy_u8ArrID[ID_Number][ID_Length],
 		u8 * Copy_PtrFlag);
-u8 u8CheckPass(u8 *Copy_u8Ptr, u8 Copy_u8ArrPass[PASS_Number][PASS_Length]);
+u8 u8CheckPass(u8 *Copy_u8Ptr, u8 Copy_u8ArrPass[PASS_Number][PASS_Length], u8 * Copy_PtrFlag);
 
 int main(void) {
 	u8 LOC_u8ComeData = 0;
 	u8 LOC_u8Flags = 0; // --> BIT No.0 Flag for ID and BIT No.1 Flag for Password
 	u8 LOC_u8Counter = 0;
+	u8 LOC_u8Counter2 = 0;
 	u8 LOC_u8arrID[ID_Number][ID_Length] = { { 'A', 'B', 'C' },
 			{ 'C', 'B', 'A' }, { 'A', 'C', 'B' } };
 	u8 LOC_u8arrPASS[PASS_Number][PASS_Length] = { { '1', '2', '3' },
 			{ '3', '2', '1' }, { '1', '3', '2' } };
 	u8 LOC_u8Enter[16] = { 0 };
+
+	u8 LOC_u8Iterator = 0;
+	u8 LOC_u8Iterator2 = 0;
+	DIO_enumSetPinDirection(DIO_PORTD_PIN7,DIO_OUTPUT);
 
 	Keypad_voidInit();
 	CLCD_voidInit();
@@ -116,9 +117,22 @@ int main(void) {
 		CLCD_voidSendString((u8 *) "Enter Password: ");
 		CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_1);
 		while (!GET_BIT(LOC_u8Flags, Pass_Flag)) {
+			if(LOC_u8Counter2 == Tries){
+				DIO_enumSetPinValue(DIO_PORTD_PIN7,DIO_HIGH);
+				CLCD_voidClearDisplay();
+				CLCD_voidSetPosition(CLCD_ROW_1, CLCD_COL_1);
+				CLCD_voidSendString((u8 *) "Wrong Password");
+				CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_1);
+				CLCD_voidSendString((u8 *) "No More Tries!!!");
+				_delay_ms(2000);
+				CLCD_voidDisplayOff();
+				while(1);
+			}
 			LOC_u8ComeData = Keypad_u8GetPressed();
 			if (LOC_u8ComeData != 0) {
+
 				if (LOC_u8ComeData == '#') {
+					LOC_u8Counter2++;
 					if (LOC_u8Counter > PASS_Length) {
 						voidClearArr(LOC_u8Enter);
 						LOC_u8Counter = 0;
@@ -127,41 +141,55 @@ int main(void) {
 						CLCD_voidSendString((u8 *) "Wrong Password");
 						CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_1);
 						CLCD_voidSendString((u8 *) "Remain Tries:");
+						CLCD_voidSendNumber(Tries - LOC_u8Counter2);
 						_delay_ms(2000);
 						CLCD_voidClearDisplay();
 						CLCD_voidSetPosition(CLCD_ROW_1, CLCD_COL_1);
-						CLCD_voidSendString((u8 *) "Plz Enter ur ID:");
+						CLCD_voidSendString((u8 *) "Enter Password: ");
 						CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_1);
 						continue;
 					}
 
-					else if (u8CheckID(LOC_u8Enter, LOC_u8arrID, &LOC_u8Flags)
+					else if (u8CheckPass(LOC_u8Enter, LOC_u8arrPASS, &LOC_u8Flags)
 							== 1) {
-						SET_BIT(LOC_u8Flags, ID_Flag);
+						SET_BIT(LOC_u8Flags, Pass_Flag);
 						voidClearArr(LOC_u8Enter);
 						LOC_u8Counter = 0;
+						CLCD_voidClearDisplay();
+						CLCD_voidSetPosition(CLCD_ROW_1,CLCD_COL_1);
+						CLCD_voidSendString((u8 *)"Welcome ");
+						for(LOC_u8Iterator = 0 ; LOC_u8Iterator< ID_Length ; LOC_u8Iterator++){
+							if(GET_BIT(LOC_u8Flags,LOC_u8Iterator)){
+								for(LOC_u8Iterator2 = 0 ; LOC_u8Iterator2 < ID_Length ; LOC_u8Iterator2++){
+								CLCD_voidSendData(LOC_u8arrID[LOC_u8Iterator][LOC_u8Iterator2]);
+								}
+							}
+						}
+						while(1);
 						break;
 					}
 
 					else {
 						voidClearArr(LOC_u8Enter);
+						voidClearArr(LOC_u8Enter);
 						LOC_u8Counter = 0;
 						CLCD_voidClearDisplay();
 						CLCD_voidSetPosition(CLCD_ROW_1, CLCD_COL_1);
-						CLCD_voidSendString((u8 *) "Wrong ID");
+						CLCD_voidSendString((u8 *) "Wrong Password");
 						CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_1);
-						CLCD_voidSendString((u8 *) "Plz Try Again");
+						CLCD_voidSendString((u8 *) "Remain Tries:");
+						CLCD_voidSendNumber(Tries - LOC_u8Counter2);
 						_delay_ms(2000);
 						CLCD_voidClearDisplay();
 						CLCD_voidSetPosition(CLCD_ROW_1, CLCD_COL_1);
-						CLCD_voidSendString((u8 *) "Plz Enter ur ID:");
+						CLCD_voidSendString((u8 *) "Enter Password: ");
 						CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_1);
 						continue;
 					}
 				}
 				LOC_u8Enter[LOC_u8Counter] = LOC_u8ComeData;
 				LOC_u8Counter++;
-				CLCD_voidSendData(LOC_u8ComeData);
+				CLCD_voidSendData('*');
 
 			}
 		}
@@ -174,9 +202,12 @@ int main(void) {
 u8 u8CheckID(u8 *Copy_u8Ptr, u8 Copy_u8ArrID[ID_Number][ID_Length],
 		u8 * Copy_PtrFlag) {
 	u8 LOC_u8Result = 0;
-	for (u8 LOC_u8Iterator = 0; LOC_u8Iterator < ID_Length; LOC_u8Iterator++) {
+	for (u8 LOC_u8Iterator = 0; LOC_u8Iterator < ID_Length + 1; LOC_u8Iterator++) {
 		if (LOC_u8Result == 1) {
 			SET_BIT(*Copy_PtrFlag, LOC_u8Iterator - 1);
+			break;
+		}
+		if (LOC_u8Iterator > 2){
 			break;
 		}
 		for (u8 LOC_u8Iterator2 = 0; LOC_u8Iterator2 < ID_Number;
@@ -198,4 +229,23 @@ void voidClearArr(u8 *Copy_u8Ptr) {
 	for (u8 LOC_u8Iterator = 0; LOC_u8Iterator < ID_Length; LOC_u8Iterator++) {
 		Copy_u8Ptr[LOC_u8Iterator] = 0;
 	}
+}
+
+
+u8 u8CheckPass(u8 *Copy_u8Ptr, u8 Copy_u8ArrPass[PASS_Number][PASS_Length], u8 * Copy_PtrFlag){
+	u8 LOC_u8Result = 1;
+	for (u8 LOC_u8Iterator = 0; LOC_u8Iterator < PASS_Length; LOC_u8Iterator++) {
+		if(GET_BIT(*Copy_PtrFlag,LOC_u8Iterator)){
+			for(u8 LOC_u8Iterator2 = 0; LOC_u8Iterator2 < PASS_Length ; LOC_u8Iterator2++){
+				if(Copy_u8ArrPass[LOC_u8Iterator][LOC_u8Iterator2] == Copy_u8Ptr[LOC_u8Iterator2]){
+					continue;
+				}
+				else{
+					LOC_u8Result = 0;
+					break;
+				}
+			}
+		}
+	}
+	return LOC_u8Result;
 }
